3f650c8d91fd8960d0be7edb67e274c0c637cd24
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-08-06 09:46:56 -0600

    Add code

--- src/gifs/api/giphy.api.ts
+++ src/gifs/api/giphy.api.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
-// vs fechapi
 
 export const giphyApi = axios.create({
   baseURL: 'https://api.giphy.com/v1/gifs',
   params: {
     lang: 'es',
-    api_key: import.meta.env.VITE_GIPHY_API_KEY,
-  },
+    api_key: import.meta.env.VITE_GIPHY_API_KEY2 || 'IdvFByhuq0EcabV8oUh8kjLd9Eguiuz4' // Default key for development
+  }
 });
